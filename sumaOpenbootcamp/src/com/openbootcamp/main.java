7c13c8acde28d50fb4f60de0929affaa5f779f9f
artssoria <53129227+artssoria@users.noreply.github.com>
2022-07-20 22:34:02 -0300

    suma de otro repositorio

---
+++ sumaOpenbootcamp/src/com/openbootcamp/main.java
@@ -0,0 +1,14 @@
+
+package com.openbootcamp;
+
+
+public class main {
+    public static void main(String[] args) {
+        suma(10,20,30);
+    }
+    public static void suma(int a, int b, int c){
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
+    }
+}
