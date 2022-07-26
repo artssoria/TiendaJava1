2d67f9696c119f6713dff5428e3091b8bd9cccb0
artssoria <53129227+artssoria@users.noreply.github.com>
2022-07-26 03:07:37 -0300

    commit clase POO persona

---
+++ ClasesPersonas/src/Personas/PersonasTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personas;
+
+/**
+ *
+ * @author arturo
+ */
+public class PersonasTest {
+    public static void main(String[] args) {
+        Persona p1 = new Persona ("27 años", "Arturo", "39984998");
+        
+        p1.DespliegaInfo();
+    }
+}
